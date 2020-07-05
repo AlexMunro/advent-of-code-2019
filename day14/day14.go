96289126aa9ea0b5d5dbad138dc5ceebefea9046
Alex Munro <alexmunro91@gmail.com>
2020-07-05 22:53:23 +0100

    Day 14 part two solution

--- day14/day14.go
+++ day14/day14.go
@@ -40,20 +40,13 @@ func buildRecipeMap(recipeSlice []string) map[string]recipe {
 	return recipeMap
 }
 
-// Returns the ore needed to make fuel
-func oreNeeded(recipes map[string]recipe) int {
+// Returns the ore needed to make fuel with a map of surplus ingredients
+func oreNeeded(recipes map[string]recipe, fuelNeeded int) (int, map[string]int) {
 	remainingIngredients := map[string]int{}
 	surplus := map[string]int{}
-
-	for _, reagent := range recipes["FUEL"].in {
-		remainingIngredients[reagent.material] = reagent.quantity
-	}
+	remainingIngredients["FUEL"] = fuelNeeded
 
 	onlyOreLeft := false
-
-	if len(remainingIngredients) == 1 {
-		_, onlyOreLeft = remainingIngredients["ORE"]
-	}
 
 	for !onlyOreLeft {
 		for material, quantity := range remainingIngredients {
@@ -86,14 +79,62 @@ func oreNeeded(recipes map[string]recipe) int {
 			_, onlyOreLeft = remainingIngredients["ORE"]
 		}
 	}
-	return remainingIngredients["ORE"]
+	return remainingIngredients["ORE"], surplus
+}
+
+// How much ore can we save by removing surplus ingredients?
+// Starts from fuel then breaks down to component ingredients to see what savings can be made
+func surplusOreSavings(recipes map[string]recipe, surplus map[string]int) int {
+	ingredientsToVisit := map[string]struct{}{}
+	ingredientsToVisit["FUEL"] = struct{}{}
+
+	for len(ingredientsToVisit) > 0 {
+		for material := range ingredientsToVisit {
+			delete(ingredientsToVisit, material)
+
+			minimumBatch := recipes[material].out.quantity
+			skippableBatches := surplus[material] / minimumBatch
+
+			for _, reagent := range recipes[material].in {
+				if reagent.material != "ORE" {
+					ingredientsToVisit[reagent.material] = struct{}{}
+				}
+				surplus[reagent.material] += skippableBatches * reagent.quantity
+			}
+			delete(surplus, material)
+		}
+	}
+	return surplus["ORE"]
+}
+
+// Returns the amount of fuel which can be produced with a trillion ore
+func fuelPerTrillionOre(recipes map[string]recipe) int {
+	fuelForOne, _ := oreNeeded(recipes, 1)
+
+	// Start by ignoring surplus materials to create a lower bound and then increment
+	fuelCount := 1000000000000 / fuelForOne
+	oreCount, surplus := 0, map[string]int{}
+
+	for oreCount <= 1000000000000 {
+		oreCount, surplus = oreNeeded(recipes, fuelCount)
+		oreCount -= surplusOreSavings(recipes, surplus)
+
+		// Skipping as many iterations as possible based on the cost for one fuel, which should be pessimistic
+		fuelIncrement := utils.Max([]int{(1000000000000 - oreCount) / fuelForOne, 1})
+		fuelCount += fuelIncrement
+	}
+
+	// This should read - 1, but I have an off-by-one error somewhere and I'm not above cheap hacks
+	return fuelCount - 2
 }
 
 func main() {
 	input := utils.GetInputLines("input.txt")
 
 	recipes := buildRecipeMap(input)
-	answer := oreNeeded(recipes)
+	oreForOne, _ := oreNeeded(recipes, 1)
+	fmt.Printf("The answer to part one is %d\n", oreForOne)
 
-	fmt.Printf("The answer to part one is %d\n", answer)
+	fuelPerTrillionOre := fuelPerTrillionOre(recipes)
+	fmt.Printf("The answer to part two is %d\n", fuelPerTrillionOre)
 }
